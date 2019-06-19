3d458dc598fb0159e564ad60f666382f12abf207
Francis Chen <fncis.chen@gmail.com>
2019-06-19 10:59:24 -0700

    added vendors and market stall URL on nav bar

--- market-org/src/components/navbar/Navbar.js
+++ market-org/src/components/navbar/Navbar.js
@@ -181,6 +181,16 @@ function ButtonAppBar(props) {
     props.history.push('/markets')
   }
 
+  const toMyStalls = (user_type) => {
+    if (user_type == "vendor") {
+      props.history.push('/vendorStall')
+    }
+    else if (user_type == "market") {
+      props.history.push('/marketStall')
+    }
+    
+  }
+
   const toCart = () => {
     let firebase_id = localStorage.getItem('firebaseId')
       props.history.push(`cart/${firebase_id}`)
@@ -359,7 +369,7 @@ console.log({currentUser}, 'currentuser')
             className={ currentUser ? null : classes.closed}
            >         
            <StyledMenuItem className={classes.menuItem} onClick={handleRegOpen}>View Profile</StyledMenuItem>
-            <StyledMenuItem className={classes.menuItem} onClick={handleRegOpen}>{user_type === 'vendor' ? 'My Stalls' : 'My Orders'}</StyledMenuItem>
+            <StyledMenuItem className={classes.menuItem} onClick={() => toMyStalls(user_type)}>{user_type === 'vendor' ? 'My Stalls' : 'My Orders'}</StyledMenuItem>
             <StyledMenuItem className={classes.menuItem} onClick={toAllVendors}>Account Settings</StyledMenuItem>
             <StyledMenuItem className={classes.menuItem} onClick={logout}>Logout</StyledMenuItem>
           </StyledMenu>
