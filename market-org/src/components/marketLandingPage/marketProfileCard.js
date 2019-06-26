4d81fba98b0c753f87b6037076fca4e555c9e4b8
Patrick <patrick.schwindt14@gmail.com>
2019-06-26 13:32:17 -0600

    Market List

--- market-org/src/components/marketLandingPage/marketProfileCard.js
+++ market-org/src/components/marketLandingPage/marketProfileCard.js
@@ -11,23 +11,19 @@ const firebase_id = null;
 const MarketProfileCard = props => {
   return (
     <div className="market-card-wrapper">
-      <div className="market-info">
+      <div className="market-left">
+        {props.profile.image ? (
+          <img alt="Market profile picture" src={props.profile.image} />
+        ) : (
+          <img alt="Market profile picture" src={fruitstand} />
+        )}
+        
+      </div>
+      <div className="market-card-right">
         <h2>{" " + props.profile.market_name}</h2>
-
-        {props.profile.image ? <img
-          alt="Market profile picture"
-          src={props.profile.image}
-          style={{ maxWidth: 250, maxHeight: 250 }}
-        /> : <img
-            alt="Market profile picture"
-            src={fruitstand}
-            style={{ width: 250, height: 300 }}
-          /> }
-        {/* <img
-          alt="Market profile picture"
-          src={props.profile.image}
-          style={{ maxWidth: 200, maxHeight: 200 }}
-        /> */}
+        <h3>
+          {" " + props.profile.phone_number}
+        </h3>
         <h4>
           Address:
           {" " + props.profile.address}
@@ -37,29 +33,26 @@ const MarketProfileCard = props => {
           {" " + props.profile.city}
           {" " + props.profile.state}
         </h4>
-        <h4>
-          Contact Info:
-          {" " + props.profile.phone_number}
-        </h4>
-      </div>
-      <div className="market-card-link">
-        <Link
-          to={{
-            pathname: "/stalls/",
-            search: `?firebase_id=${props.profile.firebase_id}`,
-            state: {
-              firebase_id: props.profile.firebase_id,
-              market_name: props.profile.market_name
-            }
-          }}
-        >
-          <h4>Rent Stalls</h4>
-        </Link>
-      </div>
-      <div className="market-card-link">
-        <Link to={`/vendorsByMarket/${props.profile.firebase_id}`}>
-          <h4>See Profile & Vendors</h4>
-        </Link>
+        
+        
+          <Link
+            to={{
+              pathname: "/stalls/",
+              search: `?firebase_id=${props.profile.firebase_id}`,
+              state: {
+                firebase_id: props.profile.firebase_id,
+                market_name: props.profile.market_name
+              }
+            }}
+          >
+            Rent Stalls
+          </Link>
+        
+        
+          <Link to={`/vendorsByMarket/${props.profile.firebase_id}`}>
+            See Profile & Vendors
+          </Link>
+        
       </div>
     </div>
   );
